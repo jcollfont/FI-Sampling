d44ab4d18d4ee7dfd2c25ef1a88ff99931d6c693
Eric Holtzman <ericholtzman93@Erics-MacBook-Pro.local>
2017-06-26 23:17:46 -0400

    Changed data, mu, sigma and a couple other variables to cells so that they were easier to iterate through. That dropped the code down to just above 100 lines and it still outputs relatively nice plots but doesn't have a legend anymore (as I haven't bothered to figure out how to best make a legend iterative). I definitely still need to go through and clean up variable names as I'm currently mixing styles very badly. Will clean up in the near future (preferably before switching to a logistic regression decision model.

--- ClusterImageGenerator3.m
+++ ClusterImageGenerator3.m
@@ -1,29 +1,45 @@
 %Generate gaussian spread random images
-c_total=2; %total number of classes
+c_total=4; %total number of classes
 imagedim=256;
 imagelabels=zeros(imagedim,imagedim);
 for i1=1:imagedim
     for i2=1:imagedim
         if i1<floor(imagedim/2)
-            imagelabels(i1,i2)=1;
+            if i2<floor(imagedim/2)
+                imagelabels(i1,i2)=1;
+            else
+                imagelabels(i1,i2)=2;
+            end
         else
-            imagelabels(i1,i2)=2;
+            if i2<floor(imagedim/2)
+                imagelabels(i1,i2)=3;
+            else
+                imagelabels(i1,i2)=4;
+            end
         end
     end
 end
 
 image1=imagelabels;
-mu1=imagedim/5;%(imagedim).*rand;
-mu2=(imagedim/5)*4;%(imagedim).*rand;
-sigma1=20;
-sigma2=10;
+mu{1}=180;%(imagedim).*rand;
+mu{2}=200;%(imagedim).*rand;
+mu{3}=100;
+mu{4}=50;
+sigma{1}=10;
+sigma{2}=10;
+sigma{3}=20;
+sigma{4}=50;
 
 for i1=1:imagedim
     for i2=1:imagedim
         if image1(i1,i2)==1
-            image1(i1,i2)=normrnd(mu1,sigma1);
+            image1(i1,i2)=normrnd(mu{1},sigma{1});
         elseif image1(i1,i2)==2
-            image1(i1,i2)=normrnd(mu2,sigma2);
+            image1(i1,i2)=normrnd(mu{2},sigma{2});
+        elseif image1(i1,i2)==3
+            image1(i1,i2)=normrnd(mu{3},sigma{3});
+        elseif image1(i1,i2)==4
+            image1(i1,i2)=normrnd(mu{4},sigma{4});
         end
         if image1(i1,i2)<1
             image1(i1,i2)=1;
