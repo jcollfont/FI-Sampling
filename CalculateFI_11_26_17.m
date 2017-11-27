efcfaa5b1a69cf65e7a0610a1bbbb7fc938c8e5d
eholtzman <ericholtzman93@gmail.com>
2017-11-27 07:13:34 -0500

    Adding updated script for Black and White Test

---
+++ CalculateFI_11_26_17.m
@@ -0,0 +1,26 @@
+function [A, EstimatedUnlabeleds] = CalculateFI_11_26_17(UnlabeledIndices, feature_map, flatFeature_map, Fit, c_total)
+EstimatedUnlabeleds=zeros(length(UnlabeledIndices),1);
+A=zeros(size(feature_map,3)+1,size(feature_map,3)+1,length(UnlabeledIndices)); %Create zeros for FI matrix
+%x=zeros(1,length(UnlabeledIndices));
+for i=1:length(UnlabeledIndices) %walk through unlabeled points
+    x=flatFeature_map(UnlabeledIndices(i),:); %at unlabeled point x
+    [y, p] = multinomial_logistic_prediction(Fit, x');
+    EstimatedUnlabeleds(i)=y;
+    S=zeros(1,c_total);
+    for c=1:c_total
+        P=p(c);
+        g=(1-P)*x;
+        dLop=g*g'; %outer product
+        S(c)=P*dLop;
+    end
+    A(:,:,i)=sum(S); %FI at x is outer product times posterior estimate summed over classes
+end
+
+
+
+
+
+
+
+
+
