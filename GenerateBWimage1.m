edc83e62d14a3c58891a9eb739bf643ae467382b
Eric Holtzman <ericholtzman93@gmail.com>
2017-12-17 12:04:45 -0500

    Delete GenerateBWimage1.m
    Deleting useless files.

--- GenerateBWimage1.m
+++
@@ -1,7 +0,0 @@
-A=ones(100)*256;
-A(1:floor(end/2),1:floor(end/2))=1;
-B=mat2gray(A);
-imwrite(B,'BWtest.jpg');
-seg=ones(100)*2;
-seg(1:floor(end/2),1:floor(end/2))=1;
-save('BWtestTruth.mat','seg','-v7.3');
